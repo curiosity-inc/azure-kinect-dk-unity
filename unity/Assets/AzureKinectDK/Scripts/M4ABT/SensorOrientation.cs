e48bc3fbc6df8359601d7683bf474898c9542ae3
Papenfuß Chris <chris.papenfuss@data-experts.de>
2019-10-19 00:18:17 +0200

    Migrated to k4a 1.3 and k4abt 0.9.4

---
+++ unity/Assets/AzureKinectDK/Scripts/M4ABT/SensorOrientation.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Azure.Kinect.Sensor
+{
+    [Native.NativeReference("k4abt_sensor_orientation_t ")]
+    public enum SensorOrientation
+    {
+        /// <summary>
+        /// Mount the sensor at its default orientation.
+        /// </summary>
+        K4ABT_SENSOR_ORIENTATION_DEFAULT = 0,
+        /// <summary>
+        /// Clockwisely rotate the sensor 90 degree.
+        /// </summary>
+        K4ABT_SENSOR_ORIENTATION_CLOCKWISE90,
+        /// <summary>
+        /// Counter-clockwisely rotate the sensor 90 degrees.
+        /// </summary>
+        K4ABT_SENSOR_ORIENTATION_COUNTERCLOCKWISE90,
+        /// <summary>
+        /// Mount the sensor upside-down.
+        /// </summary>
+        K4ABT_SENSOR_ORIENTATION_FLIP180,
+
+    }
+}
--- unity/Assets/AzureKinectDK/Scripts/M4ABT/Skeleton.cs
+++ unity/Assets/AzureKinectDK/Scripts/M4ABT/Skeleton.cs
@@ -14,6 +14,36 @@ namespace Microsoft.Azure.Kinect.Sensor.BodyTracking
         public Joint[] Joints;
     }
 
+
+    [Native.NativeReference("k4abt_joint_confidence_level_t")]
+    public enum JointConfidenceLevel
+    {
+        /// <summary>
+        /// The joint is out of range (too far from depth camera)
+        /// </summary>
+        K4ABT_JOINT_CONFIDENCE_NONE = 0,
+        /// <summary>
+        /// The joint is not observed (likely due to occlusion), predicted joint pose.
+        /// </summary>
+        K4ABT_JOINT_CONFIDENCE_LOW,
+        /// <summary>
+        /// Medium confidence in joint pose.
+        /// 
+        /// Current SDK will only provide joints up to this confidence level
+        /// </summary>
+        K4ABT_JOINT_CONFIDENCE_MEDIUM,
+        /// <summary>
+        /// High confidence in joint pose.
+        /// 
+        /// Placeholder for future SDK
+        /// </summary>
+        K4ABT_JOINT_CONFIDENCE_HIGH,
+        /// <summary>
+        /// The total number of confidence levels.
+        /// </summary>
+        K4ABT_JOINT_CONFIDENCE_LEVELS_COUNT
+    }
+
     [StructLayout(LayoutKind.Sequential)]
     [Native.NativeReference("k4abt_joint_t")]
     public struct Joint
@@ -23,5 +53,7 @@ namespace Microsoft.Azure.Kinect.Sensor.BodyTracking
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
         public float[] Orientation;
+
+        public JointConfidenceLevel confidence_level;
     }
 }
